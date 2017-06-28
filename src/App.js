e818850ff16ec5602e851a9a23db86c0eed8c4f7
Nate Hobi <nate.hobi@gmail.com>
2017-06-27 21:03:00 -0400

    test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home";
 
+//comment
 class App extends Component {
 	render() {
 		return (
